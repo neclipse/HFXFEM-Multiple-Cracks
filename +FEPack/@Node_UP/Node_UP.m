96c2f92a371f00f6f0c5f5889241bf440dc01172
Chang Huang <49694245+neclipse@users.noreply.github.com>
2020-10-02 13:51:18 -0500

    update gitignore
    add * wildcard before .asv

--- +FEPack/@Node_UP/Node_UP.m
+++ +FEPack/@Node_UP/Node_UP.m
@@ -5,11 +5,11 @@ classdef Node_UP < handle
        X
        Y
        DofArray      % global dof index array
-       EnrDofArray=cell(1,10);  % global enrdof index array
+       EnrDofArray=cell(1,3);  % global enrdof index array
        UArray        % global disp dof index array
-       UenrArray=cell(1,10);     % a cell array, same structure as the Enf
+       UenrArray=cell(1,3);     % a cell array, same structure as the Enf
        PArray        % global pressure dof index array
-       PenrArray=cell(1,10);     % a cell array, same structure as the Enf
+       PenrArray=cell(1,3);     % a cell array, same structure as the Enf
        pressure      % calculated pressure by the linear system creater
        ItrDispList   % Iterative values of dofs in corresponding order as DofArray, column array
        IncDispList   % Incremental values of dofs in corresponding order as DofArray,column array
@@ -17,15 +17,16 @@ classdef Node_UP < handle
        NoDofs
        NoUDofs
        NoPDofs
-       NoEnrDofs=zeros(1,10);    %  array, same structure as the Enrich
-       NoUenrDofs=zeros(1,10);    
-       NoPenrDofs=zeros(1,10);
+       NoEnrDofs;    %  array, same structure as the Enrich
+       NoUenrDofs;    
+       NoPenrDofs;
        Stress=zeros(5,1)         % Total stresses vector [repetition,sx,sy,sxy,sz]
        Stressp=zeros(5,1)        % Effective stresses vector [rep,sx,sy,sxy,sz]
        Leakoff=0;                % Leakoff rate
        ALeakoff=0;               % Accumulated leakoff volume
        CInjection=0;             % Concentrated injection to this node, calculated from encrack.cal_qextenr
-       Enrich=zeros(1,10);       % Enrich item id,[nan,id1,id2,...]
+       Enrich=zeros(1,3);       % Enrich item id,[nan,id1,id2,...]
+       EnrichNum=0;              % The number of enriched items involved
    end
    
    methods
@@ -54,8 +55,11 @@ classdef Node_UP < handle
             % obj.Enrich stores all the ids of involved enrichitem with
             % this element
             L=id~=obj.Enrich;
+            % Make sure this id is not already stored. Avoid duplication.
             if all(L)
-                obj.Enrich(id)=id;
+                % Store the specified id to the next available slot
+                obj.EnrichNum=obj.EnrichNum+1; 
+                obj.Enrich(obj.EnrichNum)=id;
             end
         end
        function [totndof,totudof,totpdof]=givedofarray(obj,totndof,totudof,totpdof)
